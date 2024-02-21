b00fff7a08549a9dc7690ddb759b0a93ced09e27
fathi-fayez <fathifayez155@gmail.com>
2024-02-21 19:39:54 +0200

    create the project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <h1>done1</h1>
+      <h1>done2</h1>
+      <h1>done3</h1>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <h1>dine</h1>
+      <h1>dine</h1>
+      <h1>dine</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(null);
+</script>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
