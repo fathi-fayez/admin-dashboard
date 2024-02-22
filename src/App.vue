d14318f9a9daeae5fe16c691acfefb915f022097
fathi-fayez <fathifayez155@gmail.com>
2024-02-23 01:21:49 +0200

    add side bar section and items

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,84 @@
 <template>
   <v-app id="inspire">
     <!-- drawer sidebar -->
-    <v-navigation-drawer v-model="drawer">
-      <h1>done1</h1>
-      <h1>done2</h1>
-      <h1>done3</h1>
+    <v-navigation-drawer v-model="drawer" app>
+      <!-- avatar -->
+      <v-row class="justify-center">
+        <v-col cols="auto">
+          <v-avatar>
+            <v-img
+              src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-light-atom.svg"
+            ></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col cols="auto">
+          <div class="d-flex justify-center">
+            <v-card width="300px">
+              <v-card-title class="text-h6 text-md-h5 text-lg-h4"
+                >Fathi Fayez</v-card-title
+              >
+              <v-card-text> FrontEnd developer </v-card-text>
+            </v-card>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-col cols="auto"> </v-col>
+
+      <!-- list one -->
+      <v-list>
+        <v-list-item v-for="(item, index) in itemsOne" :key="index" link>
+          <v-row>
+            <v-col cols="auto">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+            </v-col>
+            <v-col>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-col>
+          </v-row>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <!-- list two -->
+      <v-list>
+        <v-list-item v-for="(item, index) in itemsTwo" :key="index" link>
+          <v-row>
+            <v-col cols="auto">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+            </v-col>
+            <v-col>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-col>
+          </v-row>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
+      <!-- list three -->
+      <v-list>
+        <v-list-item v-for="(item, index) in itemsThree" :key="index" link>
+          <v-row>
+            <v-col cols="auto">
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+            </v-col>
+            <v-col>
+              <v-list-item-content>
+                <v-list-item-title>{{ item.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-col>
+          </v-row>
+        </v-list-item>
+      </v-list>
+      <v-divider></v-divider>
     </v-navigation-drawer>
 
     <!-- navbar -->
@@ -73,4 +147,20 @@
 import { ref } from "vue";
 
 const drawer = ref(null);
+const itemsOne = ref([
+  { text: "Dashboard", icon: "mdi-home-account" },
+  { text: "Mange Team", icon: "mdi-account-multiple" },
+  { text: "Contacts Information", icon: "mdi-contacts" },
+  { text: "Invoices Balances", icon: "mdi-receipt-text-outline" },
+]);
+const itemsTwo = ref([
+  { text: "Profile Form", icon: "mdi-account" },
+  { text: "Calendar", icon: "mdi-calendar-blank" },
+  { text: "F&Q Page", icon: "mdi-help-circle" },
+]);
+const itemsThree = ref([
+  { text: "Bar Chart", icon: "mdi-poll" },
+  { text: "Pie Chart", icon: "mdi-chart-pie" },
+  { text: "Line Chart", icon: "mdi-chart-timeline-variant" },
+]);
 </script>
